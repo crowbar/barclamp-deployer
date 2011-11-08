db25eb089b7a3a5fe29f59770d3606ae69577506
Victor Lowther <victor_lowther@dell.com>
2011-11-08 13:39:44 -0600

    Include the kernel-panic recipe in the deployer-client role.

--- chef/roles/deployer-client.rb
+++ chef/roles/deployer-client.rb
@@ -3,7 +3,8 @@ name "deployer-client"
 description "Deployer Client role - Discovery components"
 run_list(
          "recipe[ohai]",
-         "recipe[barclamp]"
+         "recipe[barclamp]",
+         "recipe[kernel-panic]"
 )
 default_attributes()
 override_attributes()
