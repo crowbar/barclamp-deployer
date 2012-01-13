f8460a849c01dfa9fd492490672ce373e5f99e13
Victor Lowther <victor_lowther@dell.com>
2012-01-13 16:57:34 -0600

    Update to make buildin tcpdump "work" on rhel 6.2
    This is fugly as heck, and I will need to revisit it at some point.

--- build_tcpdump.sh
+++ build_tcpdump.sh
@@ -5,6 +5,14 @@ bc_needs_build() {
 
 bc_build() {
     sudo cp "$BC_DIR/build_tcpdump_chroot.sh" "$CHROOT/tmp/"
+    if [[ $OS_TOKEN = redhat-6.2 ]]; then
+	cd "$BC_CACHE/$OS_TOKEN/pkgs"
+	[[ -f glibc-static-2.12-1.47.el6.x86_64.rpm ]] || \
+	    wget http://centos.mirror.lstn.net/6.2/os/x86_64/Packages/glibc-static-2.12-1.47.el6.x86_64.rpm
+	in_chroot rpm -Uvh "/mnt/$OS_TOKEN/pkgs/glibc-static-2.12-1.47.el6.x86_64.rpm"
+    elif [[ $OS_TOKEN = centos-6* ]]; then
+	in_chroot yum -y install glibc-static
+    fi
     in_chroot /tmp/build_tcpdump_chroot.sh
     cp "$CHROOT/tmp/tcpdump" "$BC_DIR/chef/cookbooks/ohai/files/default/"
 }
