a848bdad8b5e03e46dd64627579804f8c6faaed8
Greg Althaus <galthaus@austin.rr.com>
2012-02-13 12:39:06 -0800

    Remove deployer run list code and add the update state into the mix

--- crowbar_framework/app/models/deployer_service.rb
+++ crowbar_framework/app/models/deployer_service.rb
@@ -231,46 +231,6 @@ class DeployerService < ServiceObject
       save_it = true
     end
 
-## GREG: DELETE START
-## Moving into crowbar proper and run_list management
-    #
-    # The node is about to go into update.
-    # We should make sure that we save and setup the run-list for updating.
-    #
-    if state == "update" or state == "hardware-installing"
-      save_list = node.crowbar["crowbar"]["save_run_list"] || []
-      seen_bios = false
-      node.crowbar_run_list.run_list_items.each do |item|
-        if seen_bios and !(item.name =~ /^ipmi-|^raid-|^bios/)
-          save_list << item.name
-        else
-          seen_bios = true if item.name =~ /^ipmi-|^raid-|^bios/
-        end
-      end
-
-      save_list.each do |item|
-        node.crowbar_run_list.run_list_items.delete "role[#{item}]"
-      end
-
-      node.crowbar["crowbar"]["save_run_list"] = save_list
-      save_it = true
-    end
-
-    #
-    # Put the run-list back.
-    #
-    if state == "hardware-updated" or state == "hardware-installed"
-      unless node.crowbar["crowbar"]["save_run_list"].nil?
-        node.crowbar["crowbar"]["save_run_list"].each do |name|
-          node.crowbar_run_list.run_list_items << "role[#{name}]"
-        end
-      end
-
-      node.crowbar["crowbar"]["save_run_list"] = []
-      save_it = true
-    end
-## GREG: DELETE STOP
-
     node.save if save_it
 
     @logger.debug("Deployer transition: leaving state for #{name} EOF")
