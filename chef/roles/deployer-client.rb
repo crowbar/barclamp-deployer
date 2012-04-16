7949238486163dbb2793f279c4a653481a6e30eb
David Paterson <david_paterson@dell.com>
2012-04-16 16:39:37 -0400

    Added prereq recipe to role for installing rpms and gems for wsman.

--- chef/roles/deployer-client.rb
+++ chef/roles/deployer-client.rb
@@ -3,9 +3,10 @@ name "deployer-client"
 description "Deployer Client role - Discovery components"
 run_list(
          "recipe[barclamp]",
+         "recipe[prereq]",
          "recipe[repos]",
          "recipe[ohai]",
-         "recipe[kernel-panic]"
+         "recipe[kernel-panic]" 
 )
 default_attributes()
 override_attributes()
