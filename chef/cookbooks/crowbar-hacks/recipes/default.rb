4adfdb9210e9c300215ad3e2f9496c863fc018b8
Vincent Untz <vuntz@suse.com>
2013-05-24 13:49:22 +0200

    crowbar-hacks: Update for new crowbar_join log location

--- chef/cookbooks/crowbar-hacks/recipes/default.rb
+++ chef/cookbooks/crowbar-hacks/recipes/default.rb
@@ -57,7 +57,7 @@ if states.include?(node[:state])
     owner "root"
     group "root"
     mode "0644"
-    variables(:logfiles => ["/var/log/crowbar-*.log","/var/log/crowbar-*.err"])
+    variables(:logfiles => ["/var/log/crowbar/crowbar_join/*"])
   end unless node[:recipes].include?("crowbar")
 end
 
