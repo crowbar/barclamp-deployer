73bd046f9c175fcb9668d11762e679f34ed26ad3
Victor Lowther <victor.lowther@gmail.com>
2013-09-06 11:14:17 -0500

    Clear unneeded cruft from the deployer-client role.

--- chef/roles/deployer-client.rb
+++ chef/roles/deployer-client.rb
@@ -1,25 +1,11 @@
-
 name "deployer-client"
 description "Deployer Client role - Discovery components"
 run_list(
          "recipe[barclamp]",
-         "recipe[repos]",
          "recipe[crowbar-hacks]",
          "recipe[ohai]",
          "recipe[kernel-panic]"
 )
-default_attributes "deployer" => {
-  "use_allocate" => true,
-  "ignore_address_suggestions" => false,
-  "bios_map" => {
-    "pattern" => ".*",
-    "bios_set" => "Virtualization",
-    "raid_set" => "SingleRaid10"
-  },
-  "os_map" => [ { "pattern" => ".*", "install_os" => "default_os" } ],
-  "config" => {
-    "environment" => "deployer-config-default"
-  }
-}
+default_attributes()
 override_attributes()
 
