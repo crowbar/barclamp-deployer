06125ece5fcef410a2bcac71116e666689d51464
Balazs Kutil <bkutil@suse.com>
2015-01-13 15:23:35 +0100

    Revert "get_supported_speeds returns the default list"
    This reverts commit 3b620217bcc0282c8686bd1b4e968141a2fe570a.
    
    We use ruby version not affected by this.

--- chef/cookbooks/ohai/files/default/plugins/crowbar.rb
+++ chef/cookbooks/ohai/files/default/plugins/crowbar.rb
@@ -80,9 +80,6 @@ end
 
 def get_supported_speeds(interface)
   begin
-    # This raise hides segfault on ifreq.unpack. The rescue will ensure we
-    # return the defaults.
-    raise "FIXME"
     ecmd = EthtoolCmd.new
     ecmd.cmd = ETHTOOL_GSET
 
